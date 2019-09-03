475090b0f8c52c517ca7d0e55f4761cc25003054
Leonardo <leonardo.curaca@tecsup.edu.pe>
2019-09-03 16:55:24 -0500

    ultimo commit

--- src/main/java/com/tecsup/lab01/view/UserView.java
+++ src/main/java/com/tecsup/lab01/view/UserView.java
@@ -10,6 +10,7 @@ public class UserView
 {
     public static void main( String[] args )
     {
-        
+        UserController user = new UserController();
+        System.out.println(user.datos());
     }
 }
